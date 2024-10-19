4d68ee8ade916eeda70cc129732560011ddb9ad8
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-10-19 19:26:34 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Varun Singh on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
